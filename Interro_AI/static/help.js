7ffc5d118893074a1614a7979602a570bf8ca730
Romain M <73898776+Morelromain@users.noreply.github.com>
2023-01-11 17:46:26 +0100

    add javascript for help

---
+++ Interro_AI/static/help.js
@@ -0,0 +1,5 @@
+var myLink = document.getElementById("myLink");
+var explication = document.getElementById("explication");
+myLink.addEventListener("click", function(){
+    explication.style.display = "block";
+});
